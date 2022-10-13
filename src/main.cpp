8e56da6a1ad19d0fe7624e6494c12c419f1eb3f0
sami-theion <sami@theion.de>
2022-10-13 18:32:59 +0200

    time adjustement needed

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,20 @@ cnt_controller controller;
 int main()
 {
     controller.cnt_controller_connect();
-    Sleep(500); // debug sleep
+    Sleep(1000); // debug sleep
     controller.cnt_motion_unlock();
-    Sleep(500); // debug sleep
+    Sleep(2000); // debug sleep
     controller.cnt_motion_move_home();
-    Sleep(500); // debug sleep
+    Sleep(5000); // debug sleep
     controller.cnt_motion_move_to_center();
-    Sleep(500); // debug sleep
+    Sleep(5000); // debug sleep
     controller.cnt_dispenser_activate();
-    Sleep(500); // debug sleep
+    Sleep(1000); // debug sleep
     controller.cnt_dispenser_vibrate();
-    Sleep(500); // debug sleep
+    Sleep(2000); // debug sleep
     controller.cnt_dispenser_deactivate();
-    Sleep(500); // debug sleep
+    Sleep(1000); // debug sleep
     controller.cnt_motion_move_home();
+    Sleep(3000);
     return 0;
 }
